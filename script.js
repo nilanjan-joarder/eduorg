2e3f90296d029c2b9da144d5e01b78e50774b76e
nilanjan-joarder <joardernilanjan@gmail.com>
2024-07-12 03:48:55 +0530

    preloader and popup added

--- script.js
+++ script.js
@@ -13,3 +13,32 @@ function toggleMenu() {
         
     }
 }
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
+
+
+window.addEventListener("load", () => {
+    document.querySelector(".preloader").style.opacity = "0";
+    let loader = setTimeout(() => {
+        document.querySelector(".preloader").style.display = "none";
+    }, 2000)
+
+    setTimeout(function() {
+        document.getElementById('popup').style.display = 'flex';
+    }, 6000);
+
+    document.getElementById('closePopup').addEventListener('click', function() {
+        document.getElementById('popup').style.display = 'none';
+    });
+    // document.querySelector(".preloader").style.transform = "translateY(-100vh)"
+})
+
+
+
